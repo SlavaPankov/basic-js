cbcce5abdc7b6f34a4a733d06e4ea7dff937e4e1
Slava <slavapank94@yandex.ru>
2023-04-14 12:06:12 +0600

    feat: implement carbon-dating & count-cats

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -2,24 +2,36 @@ const { NotImplementedError } = require('../extensions/index.js');
 
 const MODERN_ACTIVITY = 15;
 const HALF_LIFE_PERIOD = 5730;
+const K = 0.693 / HALF_LIFE_PERIOD;
 
 /**
  * Determine the age of archeological find by using
  * given MODERN_ACTIVITY and HALF_LIFE_PERIOD values
- * 
- * @param {String} sampleActivity string representation of current activity 
+ *
+ * @param {String} sampleActivity string representation of current activity
  * @return {Number | Boolean} calculated age in years or false
  * in case of incorrect sampleActivity
  *
  * @example
- * 
+ *
  * dateSample('1') => 22387
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if (!sampleActivity || typeof sampleActivity !== 'string') {
+    return false;
+  }
+
+  const numberSampleActivity = Number(sampleActivity);
+console.log(numberSampleActivity)
+
+
+  if (numberSampleActivity === Infinity || isNaN(numberSampleActivity) || numberSampleActivity > MODERN_ACTIVITY || numberSampleActivity <= 0) {
+    return false;
+  }
+
+  return Math.abs(Math.ceil(Math.log(MODERN_ACTIVITY / numberSampleActivity) / K));
 }
 
 module.exports = {
