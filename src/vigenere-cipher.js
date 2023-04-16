c2976db31fe64f224731a703fa33adfb0a064043
Slava <slavapank94@yandex.ru>
2023-04-16 18:44:15 +0600

    feat: implement vigenere-cipher solution

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -3,30 +3,80 @@ const { NotImplementedError } = require('../extensions/index.js');
 /**
  * Implement class VigenereCipheringMachine that allows us to create
  * direct and reverse ciphering machines according to task description
- * 
+ *
  * @example
- * 
+ *
  * const directMachine = new VigenereCipheringMachine();
- * 
+ *
  * const reverseMachine = new VigenereCipheringMachine(false);
- * 
+ *
  * directMachine.encrypt('attack at dawn!', 'alphonse') => 'AEIHQX SX DLLU!'
- * 
+ *
  * directMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => 'ATTACK AT DAWN!'
- * 
+ *
  * reverseMachine.encrypt('attack at dawn!', 'alphonse') => '!ULLD XS XQHIEA'
- * 
+ *
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
- * 
+ *
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(isDirect = true) {
+    this.isDirect = isDirect;
+    this.alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    this.square = this.generateSquare();
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  generateSquare() {
+    const square = [];
+
+    for (let i = 0; i < this.alphabet.length; i++) {
+      square.push(this.alphabet.slice(i).concat(this.alphabet.slice(0, i)));
+    }
+
+    return square;
+  }
+
+  encrypt(message, key) {
+    if (!message || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+
+    let result = "";
+    for(let i = 0, j = 0; i < message.length; i++){
+      let currentLetter = message[i];
+
+      if (/[a-z]/i.test(currentLetter)){
+        result += this.square[this.alphabet.indexOf(currentLetter.toUpperCase())][this.alphabet.indexOf(key[j % key.length].toUpperCase())];
+        j++;
+      } else {
+        result += currentLetter;
+      }
+    }
+
+    return this.isDirect ? result : result.split('').reverse().join('');
+  }
+
+  decrypt(message, key) {
+    if (!message || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+
+    let result = "";
+    for (let i = 0, j = 0; i < message.length; i++) {
+      let currentLetter = message[i];
+
+      const row = this.alphabet.indexOf(key[j % key.length].toUpperCase());
+      const coll = this.square[row].indexOf(message[i].toUpperCase());
+
+      if (/[a-z]/i.test(currentLetter)) {
+        result += this.alphabet[coll];
+        j++;
+      } else {
+        result += currentLetter;
+      }
+    }
+
+    return this.isDirect ? result : result.split('').reverse().join('');
   }
 }
 
